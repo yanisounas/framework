959cad93fb1c893d7b00462406b2ed13cf19f3c6
yanisounas <106371569+yanisounas@users.noreply.github.com>
2022-11-21 21:38:12 +0100

    Update startServer.php

--- startServer.php
+++ startServer.php
@@ -8,4 +8,3 @@ if (PHP_VERSION < "8.0.0")
 
 Framework\Router\RouterInitializer::getInstance(autoStart: true);
 exec("php -S localhost:8000 -t ./ ./router.php");
-//var_dump((Framework\Router\RouterInitializer::getInstance())->controllers["Controllers"] ??= json_decode( file_get_contents(__DIR__ . "/Controller/map.json"), true )["Controllers"]);
